2357ab31601dc9b0f4159ca7d0d489bccc24a15a
dave-a-ryan <daveryan145@gmail.com>
2021-10-22 14:11:05 -0500

    continued figuring this out

--- src/views/AdvancedChat.vue
+++ src/views/AdvancedChat.vue
@@ -14,53 +14,14 @@ export default {
   data() {
     return {
       friend: { first_name: "Wilma" },
-      rooms: [
-        {
-          roomId: 1,
-          roomName: "Test",
-          avatar: "assets/imgs/people.png",
-          unreadCount: 4,
-          index: 3,
-          lastMessage: {
-            content: "Last message received",
-            senderId: 1234,
-            username: "John Doe",
-            timestamp: "10:20",
-            saved: true,
-            distributed: false,
-            seen: false,
-            new: true,
-          },
-          users: [
-            {
-              _id: 1234,
-              username: "John Doe",
-              avatar: "assets/imgs/doe.png",
-              status: {
-                state: "online",
-                lastChanged: "today, 14:30",
-              },
-            },
-            {
-              _id: 4321,
-              username: "John Snow",
-              avatar: "assets/imgs/snow.png",
-              status: {
-                state: "offline",
-                lastChanged: "14 July, 20:00",
-              },
-            },
-          ],
-          typingUsers: [4321],
-        },
-      ],
+      rooms: [],
       messages: [
         {
-          _id: 7890,
-          indexId: 12092,
+          _id: 1,
+          indexId: 8,
           content: "Message 1",
-          senderId: 1234,
-          username: "John Doe",
+          senderId: 4,
+          username: "Wilma",
           avatar: "assets/imgs/doe.png",
           date: "13 November",
           timestamp: "10:20",
@@ -71,32 +32,12 @@ export default {
           deleted: false,
           disableActions: false,
           disableReactions: false,
-          // files: [
-          //   {
-          //     name: "My File",
-          //     size: 67351,
-          //     type: "png",
-          //     audio: true,
-          //     duration: 14.4,
-          //     url: "https://firebasestorage.googleapis.com/...",
-          //     preview: "data:image/png;base64,iVBORw0KGgoAA...",
-          //   },
-          // ],
+          files: [{}],
           reactions: {},
           replyMessage: {
             content: "Reply Message",
             senderId: 4321,
-            files: [
-              {
-                // name: "My Replied File",
-                // size: 67351,
-                // type: "png",
-                // audio: true,
-                // duration: 14.4,
-                // url: "https://firebasestorage.googleapis.com/...",
-                // preview: "data:image/png;base64,iVBORw0KGgoAA...",
-              },
-            ],
+            files: [{}],
           },
         },
       ],
@@ -115,47 +56,53 @@ export default {
     loadChatroomData: function () {
       axios.get("/friends").then((response) => {
         this.friends = response.data;
-        console.log("test1");
         axios.get("/my-messages").then((response) => {
           this.myMessages = response.data;
-          console.log("all my messages", response.data);
-          this.rooms[0].roomName = this.friends[0].first_name;
-          this.rooms[0].avatar = this.friends[0].profile_picture;
-          this.rooms[0].lastMessage.content = this.myMessages[0].text;
-          this.rooms[0].lastMessage.senderId = this.friends[0].id;
-          this.rooms[0].lastMessage.username = this.friends[0].first_name;
-          console.log("my friends, ", this.friends);
-          this.rooms[0].users[0]._id = localStorage.user_id;
-          this.rooms[0].users[0].username = localStorage.first_name;
-          this.rooms[0].users[0].avatar = localStorage.profile_picture;
-          this.rooms[0].users[1]._id = this.friends[0].id;
-          this.rooms[0].users[1].username = this.friends[0].first_name;
-          this.rooms[0].users[1].avatar = this.friends[0].profile_picture;
-
-          console.log("this room", this.rooms[0]);
+          // loop test
+          const rooms = [];
+          this.friends.forEach((friend) => {
+            rooms.push({
+              roomId: friend.id,
+              roomName: friend.first_name,
+              avatar: friend.profile_picture,
+              unreadCount: 0,
+              index: 1,
+              typingUsers: [4321],
+              lastMessage: {
+                content: "test",
+                senderId: friend.id,
+                username: friend.first_name,
+                timestamp: "10:20",
+                saved: true,
+                distributed: false,
+                seen: false,
+                new: true,
+              },
+              users: [
+                {
+                  _id: localStorage.user_id,
+                  username: localStorage.first_name,
+                  avatar: localStorage.profile_picture,
+                  status: {
+                    state: "online",
+                    lastChanged: "14 July, 20:00",
+                  },
+                },
+                {
+                  _id: friend.id,
+                  username: friend.first_name,
+                  avatar: friend.profile_picture,
+                  status: {
+                    state: "online",
+                    lastChanged: "14 July, 20:00",
+                  },
+                },
+              ],
+            });
+          });
+          this.rooms = rooms;
         });
       });
-
-      // users: [
-      //   {
-      //     _id: 1234,
-      //     username: "John Doe",
-      //     avatar: "assets/imgs/doe.png",
-      //     status: {
-      //       state: "online",
-      //       lastChanged: "today, 14:30",
-      //     },
-      //   },
-      //   {
-      //     _id: 4321,
-      //     username: "John Snow",
-      //     avatar: "assets/imgs/snow.png",
-      //     status: {
-      //       state: "offline",
-      //       lastChanged: "14 July, 20:00",
-      //     },
-      //   },
-      // ],
     },
     loadMessages: function (friend) {
       // this.current_friend = friend;
