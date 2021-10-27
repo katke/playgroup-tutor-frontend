55756c94ddd2b75ea2c58a92995c6dc5ca91ff55
dave-a-ryan <daveryan145@gmail.com>
2021-10-27 18:24:56 -0500

    front page, login, and create account semi formatted

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,47 +1,97 @@
 <template>
   <div class="createaccount">
-    <button @click="showLogIn()">Wait, let me log in</button>
-    <h2>Create Account</h2>
-    <div>
-      <div>
-        Email:
-        <input type="text" v-model="inputParams.email" />
-      </div>
-      <div>
-        Password:
-        <input type="text" v-model="inputParams.password" />
-      </div>
-      <br />
-      <div>
-        First Name:
-        <input type="text" v-model="inputParams.first_name" />
-      </div>
-      <div>
-        Zip Code:
-        <input type="text" v-model="inputParams.zipcode" />
-      </div>
-      <div>
-        Profile Picture:
-        <input type="text" v-model="inputParams.profile_picture" />
-      </div>
-      <div>
-        About Me:
-        <input type="text" v-model="inputParams.about_me" />
-      </div>
-      <div>
-        Age:
-        <input type="number" v-model="inputParams.age" />
-      </div>
-    </div>
-    <br />
-    <div>What are your favorite formats?</div>
-    <div v-for="format in favorite_formats" v-bind:key="format.name">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" v-bind:id="format.name" v-model="format.checked" />
-        <label class="form-check-label" v-bind:for="format.name">{{ format.name }}</label>
-      </div>
-    </div>
-    <button @click="createAccount()">Sign up</button>
+    <main id="main">
+      <!-- ======= About Section ======= -->
+      <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>Create your account</h2>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-4">
+              <div>
+                <strong>Profile Picture:</strong>
+              </div>
+              <img src="assets/img/about.jpg" class="img-fluid" alt="" />
+              <div>Search for your favorite card:</div>
+              <input type="text" />
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content">
+              <h3>
+                Email:
+                <input type="text" v-model="inputParams.email" />
+              </h3>
+              <h3>
+                Password:
+                <input type="text" v-model="inputParams.password" />
+              </h3>
+              <p class="fst-italic">
+                Never tell anyone your full name or address. You should always meet strangers at public places like game
+                stores, libraries, or bars.
+              </p>
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="bi bi-rounded-right"></i>
+                      <strong>First Name:</strong>
+                      <input type="text" v-model="inputParams.first_name" />
+                    </li>
+                    <li>
+                      <i class="bi bi-rounded-right"></i>
+                      <strong>Zip code:</strong>
+                      <input type="text" v-model="inputParams.zipcode" />
+                    </li>
+                    <li>
+                      <i class="bi bi-rounded-right"></i>
+                      <strong>Profile Picture:</strong>
+                      <input type="text" v-model="inputParams.profile_picture" />
+                    </li>
+                    <li>
+                      <i class="bi bi-rounded-right"></i>
+                      <strong>A little about yourself:</strong>
+                      <input type="text" v-model="inputParams.about_me" />
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="bi bi-rounded-right"></i>
+                      <strong>Age:</strong>
+                      <input type="text" v-model="inputParams.age" />
+                    </li>
+                    <li>
+                      <div>
+                        <strong>What are your favorite formats?</strong>
+                      </div>
+                      <div v-for="format in favorite_formats" v-bind:key="format.name">
+                        <div class="form-check">
+                          <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value=""
+                            v-bind:id="format.name"
+                            v-model="format.checked"
+                          />
+                          <label class="form-check-label" v-bind:for="format.name">{{ format.name }}</label>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <button @click="createAccount()">Sign up</button>
+              <div @click="showLogIn()">
+                <a class="btn-about" href="/log-in">Wait, let me log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,22 @@
       <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
         <h1>Welcome to Playgroup Tutor!</h1>
         <h2>Find nearby MTG players and form new playgroups</h2>
-        <a href="/log-in" class="btn-about">Log in</a>
+        <button @click="showLogIn()" class="btn-about">Log in</button>
+        <div v-if="seeLogIn">
+          <div>
+            Email:
+            <input type="text" v-model="inputParams.email" />
+          </div>
+          <div>
+            Password:
+            <input type="text" v-model="inputParams.password" />
+          </div>
+          <div>
+            <button @click="signIn()">Log in</button>
+          </div>
+        </div>
         <a href="/create-account" class="btn-about">Create Account</a>
       </div>
-      <!-- <h1>Welcome to Playgroup Tutor!</h1>
-      <div>
-        <button @click="showLogIn()">Log in</button>
-        -
-        <button @click="showCreateAccount()">Create Account</button>
-      </div> -->
     </section>
     <!-- End Hero -->
   </div>
@@ -22,17 +29,40 @@
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
-    return {};
+    return {
+      inputParams: {
+        email: "wanda@test.com",
+        password: "password",
+      },
+      seeLogIn: false,
+    };
   },
   created: function () {},
   methods: {
     showLogIn: function () {
-      this.$router.push("/log-in");
+      this.seeLogIn = !this.seeLogIn;
     },
     showCreateAccount: function () {
       this.$router.push("/create-account");
+    },
+    signIn: function () {
+      console.log(this.inputParams);
+      axios.post("/sessions", this.inputParams).then((response) => {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        localStorage.setItem("user_id", response.data.user_id);
+        localStorage.setItem("email", response.data.email);
+        localStorage.setItem("first_name", response.data.first_name);
+        localStorage.setItem("profile_picture", response.data.profile_picture);
+        localStorage.setItem("latitude", response.data.latitude);
+        localStorage.setItem("longitude", response.data.longitude);
+        console.log(response.data, "Success!");
+        this.$router.push("/find-friends");
+      });
     },
   },
 };
