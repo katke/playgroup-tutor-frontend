fdeb73fdef4568a83d38c8da098cc04f003e89c3
dave-a-ryan <daveryan145@gmail.com>
2021-11-03 08:20:57 -0500

    fixed mobile dropdown menu

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <header id="header" class="fixed-top" v-if="isHome">
       <div class="container-fluid d-flex justify-content-between align-items-center">
         <h1 class="logo me-auto me-lg-0"><a href="#">Playgroup Tutor</a></h1>
-
         <nav id="navbar" class="navbar order-last order-lg-0">
+          <!-- ======= Desktop Navbar ======= -->
           <ul>
             <li><a href="/find-friends">Find Friends</a></li>
             <li><a href="/advanced-chat">Chat</a></li>
@@ -16,33 +16,51 @@
               </a>
             </li>
           </ul>
+          <!-- End Desktop Navbar -->
+          <!-- ======= Mobile Dropdown -->
+          <div class="navbar order-last order-lg-0">
+            <div class="dropdown">
+              <i class="bi bi-list mobile-nav-toggle dropdown-toggle" data-bs-toggle="dropdown" id="mobileDropdown"></i>
 
-          <i class="bi bi-list mobile-nav-toggle"></i>
+              <ul class="dropdown-menu" aria-labelledby="mobileDropdown">
+                <li><a href="/find-friends" class="dropdown-item">Find Friends</a></li>
+                <li><a href="/advanced-chat" class="dropdown-item">Chat</a></li>
+                <li>
+                  <a href="/friend-requests" class="dropdown-item">
+                    Friend Requests&nbsp;
+                    <span id="friend-requests" style="color: red">({{ friendRequests }})</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
-        <!-- .navbar -->
+        <!-- End Mobile Dropdown -->
+        <!-- ======= Login Dropdown ======= -->
         <div class="navbar order-last order-lg-0">
           <div class="dropdown" @update-login-name="loginCheck()">
             <button
               class="btn btn-secondary dropdown-toggle"
               type="button"
-              id="dropdownMenuButton2"
+              id="loginDropdown"
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
               {{ loginStatus }}
             </button>
-            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="loginDropdown">
               <li><a class="dropdown-item active" href="/me">View / Edit my Profile</a></li>
               <li><hr class="dropdown-divider" /></li>
               <li><a class="dropdown-item" @click="logOut()">Log out</a></li>
             </ul>
           </div>
         </div>
+        <!-- End Login Dropdown -->
       </div>
     </header>
     <!-- End Header -->
 
-    <!-- ROUTER VIEW -->
+    <!-- ======= Router view ======= -->
     <router-view />
 
     <!-- ======= Footer ======= -->
@@ -57,7 +75,11 @@
       </div>
     </footer>
     <!-- End  Footer -->
-    <!-- <div id="preloader"></div> -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+      <i class="bi bi-arrow-up-short"></i>
+      test
+    </a>
   </div>
 </template>
 
@@ -113,6 +135,7 @@ export default {
       this.$router.push("/me");
     },
   },
+  // headeer/footer does NOT show up on login/create pages
   computed: {
     isHome() {
       return this.$route.name !== "Home" && this.$route.name !== "Log In" && this.$route.name !== "Create Account"
