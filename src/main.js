b621efc27eb11520c05a20409bad89f14b029933
dave-a-ryan <daveryan145@gmail.com>
2021-11-17 15:33:04 -0600

    slash

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { CoolSelectPlugin } from "vue-cool-select";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://playgroup-tutor-api.herokuapp.com/";
+    : "https://playgroup-tutor-api.herokuapp.com";
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
