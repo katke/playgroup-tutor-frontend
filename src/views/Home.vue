52f50db7359987088bfd4d081abf56ea8a14a7bb
dave-a-ryan <daveryan145@gmail.com>
2021-11-22 17:17:22 -0600

    fixed vertical centering on splash, cleaning up CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,54 @@
 <template>
   <div class="home">
-    <!-- ======= Hero Section ======= -->
-    <section id="hero">
-      <div class="container">
-        <div class="row">
-          <div
-            class="d-flex flex-column align-items-center"
-            data-aos="zoom-in"
-            data-aos-delay="100"
-          >
-            <h1
-              class="d-flex align-items-center"
-              style="font-size: 65px"
-              id="splash-text"
-            >
-              Playgroup Tutor
-            </h1>
-            <br />
-            <h2 style="font-size: 28px">
-              Find nearby Magic: the Gathering players and form new playgroups
-            </h2>
-            <br />
-            <form v-on:submit.prevent="signIn()">
-              <div class="blurry_box">
-                <div class="row">
-                  <div class="col">
-                    Email
-                    <input
-                      type="text"
-                      class="form-control form-control-sm"
-                      id="EmailForm"
-                      v-model="inputParams.email"
-                    />
-                    <div class="invalid-feedback">
-                      Invalid email or password.
-                    </div>
-                    <div class="valid-feedback"></div>
-                  </div>
-                  <div class="col">
-                    Password
-                    <input
-                      type="password"
-                      class="form-control form-control-sm"
-                      id="PasswordForm"
-                      v-model="inputParams.password"
-                    />
-                    <div class="invalid-feedback"></div>
-                    <div class="valid-feedback"></div>
-                  </div>
+    <section id="splash">
+      <div class="row h-100">
+        <div
+          class="d-flex flex-column my-auto align-items-center"
+          data-aos="zoom-in"
+          data-aos-delay="100"
+        >
+          <h1 class="mb-3">Playgroup Tutor</h1>
+          <h2 class="mb-3">
+            Find nearby Magic: the Gathering players and form new playgroups
+          </h2>
+          <form v-on:submit.prevent="signIn()">
+            <div class="blurry_box">
+              <div class="row">
+                <div class="col">
+                  Email
+                  <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    id="EmailForm"
+                    v-model="inputParams.email"
+                  />
+                  <div class="invalid-feedback">Invalid email or password.</div>
+                  <div class="valid-feedback"></div>
+                </div>
+                <div class="col">
+                  Password
+                  <input
+                    type="password"
+                    class="form-control form-control-sm"
+                    id="PasswordForm"
+                    v-model="inputParams.password"
+                  />
+                  <div class="invalid-feedback"></div>
+                  <div class="valid-feedback"></div>
                 </div>
               </div>
-              <button class="d-none">
-                This is my hidden button to allow 'enter' to log you in
-              </button>
-            </form>
-            <button @click="signIn()" class="btn-about">Log in</button>
-            <button @click="showCreateAccount()" class="btn-about">
-              Create Account
+            </div>
+            <button class="d-none">
+              This is my hidden button to allow 'enter' to log you in
             </button>
-          </div>
+          </form>
+          <button @click="signIn()" class="btn-about mb-3">Log in</button>
+          <button @click="showCreateAccount()" class="btn-about">
+            Create Account
+          </button>
         </div>
       </div>
     </section>
-    <!-- End Hero -->
   </div>
 </template>
 
